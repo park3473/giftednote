e4df88c8af7c9c5ed0cda278cbedae61a62e22f5
park3473 <73814918+park3473@users.noreply.github.com>
2021-07-31 16:02:56 +0900

    Delete kaist-giftedup directory

--- kaist-giftedup/src/main/webapp/resources/js/admin/member.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".adm_menu_con > li").eq(0).find(".sub_menu_con").show();
-    $(".adm_menu_con > li").eq(0).css({
-        backgroundColor: "#fff"
-    });
-});
--- kaist-giftedup/src/main/webapp/resources/js/admin/portfolio.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".adm_menu_con > li").eq(5).find(".sub_menu_con").show();
-    $(".adm_menu_con > li").eq(5).css({
-        backgroundColor: "#fff"
-    });
-});
